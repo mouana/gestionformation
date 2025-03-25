cc4821d8cf9b54171f16feae031e93507b6f8acf
abdelhak bouzougar <abdelhak.bouzougar321@gmail.com>
2025-03-25 15:54:15 +0000

    test

---
+++ backend/app/Http/Controllers/FormateurAnimateurController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FormateurAnimateur;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FormateurAnimateurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FormateurAnimateur $formateurAnimateur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FormateurAnimateur $formateurAnimateur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FormateurAnimateur $formateurAnimateur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FormateurAnimateur $formateurAnimateur)
+    {
+        //
+    }
+}
